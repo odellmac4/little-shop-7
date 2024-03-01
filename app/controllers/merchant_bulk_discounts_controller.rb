1239d282538f8093d24951d08e770aa6856fb069
Mel Langhoff <mlanghoff@uwalumni.com>
2024-03-01 05:25:08 -0700

    add #create and #new

--- app/controllers/merchant_bulk_discounts_controller.rb
+++ app/controllers/merchant_bulk_discounts_controller.rb
@@ -10,6 +10,19 @@ class MerchantBulkDiscountsController < ApplicationController
     end
 
     def new
-        
+        @merchant = Merchant.find(params[:merchant_id])
+        @discount = Discount.new
+    end
+
+    def create
+        @merchant = Merchant.find(params[:merchant_id])
+        @discount = @merchant.discounts.create!(discount_params)
+        redirect_to merchant_discounts_path(@merchant)
+    end
+
+    private
+
+    def discount_params
+        params.permit(:percent_discount, :quantity_threshold)
     end
 end
