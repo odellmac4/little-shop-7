ccb727d45236aa8c7a5a5f339f3fb5314eb408dc
Mel Langhoff <mlanghoff@uwalumni.com>
2024-03-01 05:39:23 -0700

    add data validations

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -2,4 +2,7 @@ class BulkDiscount < ApplicationRecord
     belongs_to :merchant
     has_many :bulk_discount_items
     has_many :items, through: :bulk_discount_items
+
+    validates :percent_discount, presence: true
+    validates :quantity_threshold, presence: true
 end
--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe BulkDiscount do
-    it { should have_many(:bulk_discount_items) }
-    it { should have_many(:items).through(:bulk_discount_items) }
-    it { should belong_to(:merchant) }
+    describe 'relationships' do
+        it { should have_many(:bulk_discount_items) }
+        it { should have_many(:items).through(:bulk_discount_items) }
+        it { should belong_to(:merchant) }
+    end
+
+    describe 'validations' do
+        it { should validate_presence_of :percent_discount}
+        it { should validate_presence_of :quantity_threshold }
+    end
 end
